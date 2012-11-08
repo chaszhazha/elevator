526ca8c9cb7b68d1a57ea78eddd1bac79167aa87
Charles <charles@charles-VirtualBox.(none)>
2012-11-07 22:53:05 -0500

    simulator

---
+++ simulator.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+//**** Guest elevator status*****
+#define WAITING 0
+#define ON 1
+#define OFF 2
+//*******************************
+#define FILE_BUF_SIZE 1024
+
+typedef struct
+{
+    int request_time;
+    int at;
+    int to;
+    int status;
+    
+}guest;
+
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        fprintf(stderr, "Syntax error, usage: simulator [test file]\n");
+        return 1;
+    }
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        fprintf(stderr, "file open() error: %s\n", strerror(errno));
+        return 1;
+    }
+    char buf[FILE_BUF_SIZE];
+    int bytes_read;
+    // Consider using fgets
+    
+    /*
+    while((bytes_read = read(fd, buf, sizeof(buf))) != 0)
+    {
+        if (bytes_read == -1)
+        {
+            fprintf(stderr, "read() error: %s\n", strerror(errno));
+            return 1;
+        }
+    }*/
+    return 0;
+}
